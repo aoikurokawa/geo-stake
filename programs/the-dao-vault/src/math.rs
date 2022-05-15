e485286ba9d20214db09d243913fa6092b07f165
Aoi <ht.so.ik0102@gmail.com>
2022-05-15 17:51:05 +0700

    Update math.rs

--- programs/the-dao-vault/src/math.rs
+++ programs/the-dao-vault/src/math.rs
@@ -2,7 +2,7 @@ use std::convert::TryFrom;
 
 use anchor_lang::{
     prelude::ProgramError,
-    solana_program::clock::{DEFAULT_HASHES_PER_SECOND, DEFAULT_TICKS_PER_SLOT, SECONDS_PER_DAY},
+    solana_program::clock::{DEFAULT_TICKS_PER_SECOND, DEFAULT_TICKS_PER_SLOT, SECONDS_PER_DAY},
 };
 use spl_math::precise_number::PreciseNumber;
 
@@ -33,3 +33,26 @@ pub fn calc_reserve_to_lp(
         }
     }
 }
+
+pub fn calc_lp_to_reserve(
+    lp_token_amount: u64,
+    lp_token_supply: u64,
+    reserve_tokens_in_vault: u64,
+) -> Option<u64> {
+    let lp_token_amount = PreciseNumber::new(lp_token_amount as u128)?;
+    let lp_token_supply = PreciseNumber::new(lp_token_supply as u128)?;
+    let reserve_tokens_in_vault = PreciseNumber::new(reserve_tokens_in_vault as u128)?;
+
+    let reserve_tokens_to_transfer = lp_token_amount
+        .checked_mul(&reserve_tokens_in_vault)?
+        .checked_div(&lp_token_supply)?
+        .floor()?
+        .to_imprecise()?;
+
+    u64::try_from(reserve_tokens_to_transfer).ok()
+}
+
+/// Number of slots per year
+/// 63072000
+pub const SLOTS_PER_YEAR: u64 =
+    DEFAULT_TICKS_PER_SECOND / DEFAULT_TICKS_PER_SLOT * SECONDS_PER_DAY * 365;
