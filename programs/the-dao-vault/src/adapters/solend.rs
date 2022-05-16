5a6744d80ce0e2f0c13cf60c82cdd4cea24a47be
Aoi <ht.so.ik0102@gmail.com>
2022-05-16 16:24:06 +0700

    Solend Accounts

--- programs/the-dao-vault/src/adapters/solend.rs
+++ programs/the-dao-vault/src/adapters/solend.rs
@@ -12,5 +12,73 @@ use crate::{
     impl_has_vault,
     reconcile::LendingMarket,
     reserves::{Provider, ReserveAccessor},
-    state::Vault
+    state::Vault,
 };
+
+#[derive(Accounts)]
+pub struct SolendAccounts<'info> {
+    /// Vault state account
+    /// Checks that the accounts passed in are correct
+    #[account(mut, has_one = vault_authority, has_one = vault_reserve_token, has_one = vault_solend_lp_token, has_one = solend_reserve, )]
+    pub vault: Box<Account<'info, Vault>>,
+
+    /// Authority that the vault uses for lp token mints/burns ans transfers to/from downstream assets
+    pub vault_authority: AccountInfo<'info>,
+
+    /// Token account for the vault's reserve tokens
+    #[account(mut)]
+    pub vault_reserve_token: Box<Account<'info, TokenAccount>>,
+
+    #[account(mut)]
+    pub vault_solend_lp_token: Box<Account<'info, TokenAccount>>,
+
+    #[account(executable, address = spl_token_lending::ID)]
+    pub solend_program: AccountInfo<'info>,
+
+    pub solend_market_authority: AccountInfo<'info>,
+
+    pub solend_market: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub solend_reserve: Box<Account<'info, SolendReserve>>,
+
+    #[account(mut)]
+    pub solend_lp_mint: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub solend_reserve_token: AccountInfo<'info>,
+
+    pub clock: Sysvar<'info, Clock>,
+
+    pub token_program: Program<'info, Token>,
+}
+
+impl_has_vault!(SolendAccounts<'_>);
+
+#[derive(Clone)]
+pub struct SolendReserve(Reserve);
+
+impl anchor_lang::AccountDeserialize for SolendReserve {
+    fn try_deserialize(buf: &mut &[u8]) -> Result<Self> {
+        SolendReserve::try_deserialize_unchecked(buf)
+    }
+
+    fn try_deserialize_unchecked(buf: &mut &[u8]) -> Result<Self> {
+        match <Reserve as solana_program::program_pack::Pack>::unpack(buf).map(SolendReserve) {
+            Ok(val) => Ok(val),
+            Err(err) => Err(err.into()),
+        }
+    }
+}
+
+impl anchor_lang::AccountSerialize for SolendReserve {
+    fn try_serialize<W: Write>(&self, _writer: &mut W) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl anchor_lang::Owner for SolendReserve {
+    fn owner() -> Pubkey {
+        spl_token_lending::id()
+    }
+}
