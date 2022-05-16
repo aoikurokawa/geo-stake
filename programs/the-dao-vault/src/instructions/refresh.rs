64649c7640439927449d164280a4a177c94eccb6
Aoi <ht.so.ik0102@gmail.com>
2022-05-17 06:23:56 +0700

    Create refresh file

--- programs/the-dao-vault/src/instructions/refresh.rs
+++ programs/the-dao-vault/src/instructions/refresh.rs
@@ -5,7 +5,17 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount};
 use port_anchor_adaptor::{port_lending_id, PortReserve};
 
-use crate::{
-    adapters::{solend, SolendReserve};
-    errors::ErrorCode,
-};
+use crate::adapters::{solend, SolendReserve};
+use crate::errors::ErrorCode;
+
+pub trait Refresher<'info> {
+    fn update_actual_allocation(&mut self, remaining_accounts: &[AccountInfo]<'info>) -> Result<()>
+}
+
+/// Refreshes the reserves of downstream lending markets
+pub fn handler<'info, T: Refresher<'info>>(ctx: Context<'_, '_, '_, 'info, T>) -> Result<()>{
+    #[cfg(feature = "debug")]
+    msg!("Refreshing lending pool");
+
+    ctx.accounts.update_actual_allocation(ctx.remaining_accounts)
+}
