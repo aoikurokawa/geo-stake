e19a2a77a438b8af4bbf3b52a78bb8c23f47296b
Aoi <ht.so.ik0102@gmail.com>
2022-05-21 15:21:29 +0700

    Implement reserves.rs

--- programs/the-dao-vault/src/asset_container/reserves.rs
+++ programs/the-dao-vault/src/asset_container/reserves.rs
@@ -45,13 +45,68 @@ impl AssetContainer<Reserves> {
             .and_then(
                 |num_assets| match Rate::from_percent(num_assets).try_mul(100) {
                     Ok(v) => Ok(v),
-                    Err(err) => Err(ErrorCode::MathError.into()),
+                    Err(_) => Err(ErrorCode::MathError.into()),
                 },
             )
             .and_then(|r| match Rate::one().try_div(r) {
                 Ok(v) => Ok(v),
-                Err(err) => Err(ErrorCode::MathError.into()),
+                Err(_) => Err(ErrorCode::MathError.into()),
             })
             .map(|equal_allocation| self.apply(|_, _| equal_allocation))
     }
+
+    pub fn calculate_weights(
+        &self,
+        strategy_type: StrategyType,
+        allocation_cap_pct: u8,
+    ) -> Result<AssetContainer<Rate>> {
+        match strategy_type {
+            StrategyType::MaxYield => self.calculate_weights_max_yield(allocation_cap_pct),
+            StrategyType::EqualAllocation => self.calculate_weight_equal(),
+        }
+    }
+
+    pub fn get_apr(
+        &self,
+        weights: &dyn Index<Provider, Output = Rate>,
+        allocations: &dyn Index<Provider, Output = u64>,
+    ) -> Result<Rate> {
+        let res = self
+            .into_iter()
+            .map(|(p, r)| {
+                r.calculate_return(allocations[p])
+                    .and_then(|r| match weights[p].try_mul(r) {
+                        Ok(v) => Ok(v),
+                        Err(err) => Err(err.into()),
+                    })
+            })
+            .try_fold(Rate::zero(), |acc, r| acc.try_add(r?));
+
+        match res {
+            Ok(r) => Ok(r),
+            Err(err) => Err(err.into()),
+        }
+    }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use crate::reserves::MockReturnCalculator;
+//     use solana_maths::Rate;
+
+//     #[test]
+//     fn test_compare() {
+//         let mut mock_rc1 = MockReturnCalculator::new();
+//         mock_rc1
+//             .expect_calculate_return()
+//             .return_const(Ok(Rate::from_percent(10)));
+
+//         let mut mock_rc2 = MockReturnCalculator::new();
+//         mock_rc1
+//             .expect_calculate_return()
+//             .return_const(Ok(Rate::from_percent(20)));
+
+//         assert_eq!(compare(&mock_rc1, &mock_rc2), Ok(Ordering::Less));
+//     }
+// }
