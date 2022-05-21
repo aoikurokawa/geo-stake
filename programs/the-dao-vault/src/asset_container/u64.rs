7d409dc5cd69caec9dd9c755ce2f8b875e46b611
Aoi <ht.so.ik0102@gmail.com>
2022-05-21 15:45:01 +0700

    implement u64

--- programs/the-dao-vault/src/asset_container/u64.rs
+++ programs/the-dao-vault/src/asset_container/u64.rs
@@ -9,15 +9,36 @@ impl<const N: usize> AssetContainerGeneric<u64, N> {
     pub fn try_from_weights(
         rates: &AssetContainerGeneric<Rate, N>,
         total_amount: u64,
-    ) -> Result<()> {
+    ) -> Result<Self> {
         let res = rates.try_apply(|_, rate| {
             rate.try_mul(total_amount)
                 .and_then(|product| Decimal::from(product).try_floor_u64())
         });
 
         match res {
-            Ok(_) => Ok(()),
+            Ok(val) => Ok(val),
             Err(err) => Err(err.into()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_try_from_weights() {
+        let rates = AssetContainerGeneric::<Rate, 3> {
+            inner: [
+                Some(Rate::from_percent(10)),
+                Some(Rate::from_percent(59)),
+                Some(Rate::from_percent(100)),
+            ],
+        };
+        let expected: [u64; 3] = [20, 118, 200];
+        AssetContainerGeneric::<u64, 3>::try_from_weights(&rates, 200)
+            .unwrap()
+            .into_iter()
+            .for_each(|(p, n)| assert_eq!(n, expected[p as usize]))
     }
 }
