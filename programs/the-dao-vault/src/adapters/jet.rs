e34036e94bae4c03a5fce73e4cfc3a77922fdf09
aoikurokawa <ht.so.ik0102@gmail.com>
2024-08-07 08:44:07 +0900

    feat: update

--- programs/the-dao-vault/src/adapters/jet.rs
+++
@@ -1,8 +0,0 @@
-use std::ops::{Deref, DerefMut};
-
-use anchor_lang::prelude::*;
-use anchor_spl::token::{Token, TokenAccount};
-use solana_maths::Rate;
-
-// use crate::adapters::
-
--- programs/the-dao-vault/src/adapters/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod jet;
-pub mod port;
-pub mod solend;
-
-pub use jet::*;
-pub use port::*;
-pub use solend::*;
